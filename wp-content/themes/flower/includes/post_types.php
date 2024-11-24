83d4d3ebacef6de3be859f92186304555d0ea41b
danng <danhoat@gmail.com>
2024-11-24 12:51:21 +0700

    update slider

--- wp-content/themes/flower/includes/post_types.php
+++ wp-content/themes/flower/includes/post_types.php
@@ -40,12 +40,12 @@ function box_codex_slider_init() {
         'show_ui'            => true,
         'show_in_menu'       => true,
         'query_var'          => true,
-        'rewrite'            => array( 'slug' => 'slider' ),
+        //'rewrite'            => array( 'slug' => 'slider' ),
         'capability_type'    => 'post',
-        'has_archive'        => true,
+        'has_archive'        => false,
         'hierarchical'       => false,
         'menu_position'      => null,
-        'supports'           => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments' ),
+        'supports'           => array( 'title' ),
     );
 
     register_post_type( 'Slider', $args );
