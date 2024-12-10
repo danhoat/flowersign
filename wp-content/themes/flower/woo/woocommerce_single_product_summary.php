9dbc7f8475bf25d70bd5d426d86f9a747c900029
danng <danhoat@gmail.com>
2024-12-10 18:07:58 +0700

    ok

--- wp-content/themes/flower/woo/woocommerce_single_product_summary.php
+++ wp-content/themes/flower/woo/woocommerce_single_product_summary.php
@@ -1,11 +1,14 @@
 <?php 
 
-function woocommerce_template_single_title1(){ ?>
-   <h1 class="product_title entry-title">
+function woocommerce_template_single_title(){ ?>
+   
+   <!-- <h1 class="product_title entry-title">
        <?php the_title();?>
-   </h1> <?php
+   </h1>
+   !-->
+    <?php
 }
-add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_title1', 5 );
+add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_title', 5 );
 
 
 function woocommerce_template_single_after_meta(){
