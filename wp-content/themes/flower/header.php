e9fa90c34e1578b225c32b3862d3c3761dd96fdc
danng <danhoat@gmail.com>
2024-12-17 13:52:01 +0700

    ok

--- wp-content/themes/flower/header.php
+++ wp-content/themes/flower/header.php
@@ -10,6 +10,9 @@ window.Firstmall.Config = {"Environment":{"MobileMode":true,"SetMode":null,"Lang
 <!-- 구글 웹폰트 -->
 <link href="https://fonts.googleapis.com/css?family=Noto+Sans+KR:100,300,400,500,700&amp;subset=korean" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&family=Prompt:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
 <link rel="stylesheet" type="text/css" href="https://f-mans.com/data/skin/responsive_ver1_default_gl/css/jqueryui/black-tie/jquery-ui-1.8.16.custom.css">
 <link rel="stylesheet" type="text/css" href="https://f-mans.com/app/javascript/plugin/slick/slick.css"><!-- 반응형 슬라이드 -->
