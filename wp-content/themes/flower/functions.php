4c798c70a798bb3b077e465e88a3033313364a08
danng <danhoat@gmail.com>
2024-12-19 12:31:13 +0700

    update css

--- wp-content/themes/flower/functions.php
+++ wp-content/themes/flower/functions.php
@@ -8,8 +8,19 @@ require_once __DIR__ .'/includes/required.php';
 require_once __DIR__ .'/woo/inc.php';
 require_once __DIR__ .'/elementor/required.php';
 require_once __DIR__ .'/block/required.php';
+function testimonial_item(){?>
+
+<div class="testimonial-item">
+    <p>Great service, delivery, price and product! Quality of flowers are amazing. Will be using you for all my flower needs.</p>
+    <div class="box-image">
+        <img src = "https://demo.dichvu139.com/wp-content/plugins/elementor/assets/images/placeholder.png" >
+    </div>
+    <div class="testimonial-author">
+        <p class="testimonial-name"> Martin Micker</p>
+        <p class="testimonial-job">Physician</p>
+    </div>
+</div>
+
+<?php } ?>
 
 
-
-
-//echo BOXTHEME_URL; //http://localhost/flower/wp-content/themes/flower
