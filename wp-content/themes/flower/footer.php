90ab98d79474761d33a3af05179deac33ac7b032
danng <danhoat@gmail.com>
2024-12-24 03:36:22 +0700

    update done

--- wp-content/themes/flower/footer.php
+++ wp-content/themes/flower/footer.php
@@ -199,11 +199,11 @@ var hpt_info = {'_account':'UHPT-20592','_server':'a24'};
             <ul class="list v5">
                 <li><a href="/thong-tin/" >Thông tin</a></li>
                 <i></i>
-                <li><a href="/gioi-thieu" >Giới Thiệu</a></li>
+                <li><a href="/gioi-thieu/" >Giới Thiệu</a></li>
                 <i></i>
                 <li><a href="/partner/" >Hợp Tác</a></li>
                 <i></i>
-                <li><a href="/agreement"> Điều Khoản</a></li>
+                <li><a href="/agreement/"> Điều Khoản</a></li>
                
             </ul>
             
